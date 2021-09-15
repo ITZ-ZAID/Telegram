8fc264f5b199b2bedef6e8f650aa1288eea1ec06
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-09-15 21:15:20 +0530

    Update stickers.py

--- TGNRobot/modules/stickers.py
+++ TGNRobot/modules/stickers.py
@@ -12,8 +12,8 @@ from telegram import TelegramError, Update
 from telegram.ext import run_async, CallbackContext
 from telegram.utils.helpers import mention_html
 
-from LaylaRobot import dispatcher
-from LaylaRobot.modules.disable import DisableAbleCommandHandler
+from TGNRobot import dispatcher
+from TGNRobot.modules.disable import DisableAbleCommandHandler
 
 combot_stickers_url = "https://combot.org/telegram/stickers?q="
 
