4f03df67b858395d2ed1a030edcf342d1ac28ce3
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-09-15 20:11:55 +0530

    Update locks_sql.py

--- TGNRobot/modules/sql/locks_sql.py
+++ TGNRobot/modules/sql/locks_sql.py
@@ -3,7 +3,7 @@ import threading
 
 from sqlalchemy import Column, String, Boolean
 
-from LaylaRobot.modules.sql import SESSION, BASE
+from TGNRobot.modules.sql import SESSION, BASE
 
 
 class Permissions(BASE):
