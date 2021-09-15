ff448d047f967f2cbe57f8a7994b0855de5b6e58
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-09-15 21:18:24 +0530

    Update wallpaper.py

--- TGNRobot/modules/wallpaper.py
+++ TGNRobot/modules/wallpaper.py
@@ -1,8 +1,8 @@
 from random import randint
 
 import requests as r
-from LaylaRobot import SUPPORT_CHAT, WALL_API, dispatcher
-from LaylaRobot.modules.disable import DisableAbleCommandHandler
+from TGNRobot import SUPPORT_CHAT, WALL_API, dispatcher
+from TGNRobot.modules.disable import DisableAbleCommandHandler
 from telegram import Update
 from telegram.ext import CallbackContext, run_async
 
