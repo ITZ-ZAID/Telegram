8b0b8992c5ff6a48b537fa33e6cb791c091d8751
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-09-15 20:55:20 +0530

    Update gettime.py

--- TGNRobot/modules/gettime.py
+++ TGNRobot/modules/gettime.py
@@ -2,8 +2,8 @@ import datetime
 from typing import List
 
 import requests
-from LaylaRobot import TIME_API_KEY, dispatcher
-from LaylaRobot.modules.disable import DisableAbleCommandHandler
+from TGNRobot import TIME_API_KEY, dispatcher
+from TGNRobot.modules.disable import DisableAbleCommandHandler
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 
