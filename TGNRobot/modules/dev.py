b85ac6a0250795a667b62ac234d36b75c480c3d2
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-09-15 22:01:38 +0530

    Update dev.py

--- TGNRobot/modules/dev.py
+++ TGNRobot/modules/dev.py
@@ -18,7 +18,7 @@ from telegram.ext import CallbackContext, CommandHandler, run_async
 def allow_groups(update: Update, context: CallbackContext):
     args = context.args
     if not args:
-        update.effective_message.reply_text(f"Current state: {LaylaRobot.ALLOW_CHATS}")
+        update.effective_message.reply_text(f"Current state: {TGNRobot.ALLOW_CHATS}")
         return
     if args[0].lower() in ["off", "no"]:
         TGNRobot.ALLOW_CHATS = True
