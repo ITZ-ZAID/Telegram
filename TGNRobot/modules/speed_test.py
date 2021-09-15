9acd34085eb3a288734f28eb73bd5561a1953fcf
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-09-15 21:14:58 +0530

    Update speed_test.py

--- TGNRobot/modules/speed_test.py
+++ TGNRobot/modules/speed_test.py
@@ -1,7 +1,7 @@
 import speedtest
-from LaylaRobot import DEV_USERS, dispatcher
-from LaylaRobot.modules.disable import DisableAbleCommandHandler
-from LaylaRobot.modules.helper_funcs.chat_status import dev_plus
+from TGNRobot import DEV_USERS, dispatcher
+from TGNRobot.modules.disable import DisableAbleCommandHandler
+from TGNRobot.modules.helper_funcs.chat_status import dev_plus
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Update
 from telegram.ext import CallbackContext, CallbackQueryHandler, run_async
 
