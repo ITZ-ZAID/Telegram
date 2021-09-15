af261bf5c15ddc4d27a9ccc1a3df60d777b60577
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-09-15 20:45:09 +0530

    Update connection.py

--- TGNRobot/modules/connection.py
+++ TGNRobot/modules/connection.py
@@ -5,10 +5,10 @@ from telegram import ParseMode, InlineKeyboardMarkup, InlineKeyboardButton, Upda
 from telegram.error import BadRequest, Unauthorized
 from telegram.ext import CommandHandler, CallbackQueryHandler, run_async
 
-import LaylaRobot.modules.sql.connection_sql as sql
-from LaylaRobot import dispatcher, DRAGONS, DEV_USERS
-from LaylaRobot.modules.helper_funcs import chat_status
-from LaylaRobot.modules.helper_funcs.alternate import send_message, typing_action
+import TGNRobot.modules.sql.connection_sql as sql
+from TGNRobot import dispatcher, DRAGONS, DEV_USERS
+from TGNRobot.modules.helper_funcs import chat_status
+from TGNRobot.modules.helper_funcs.alternate import send_message, typing_action
 
 user_admin = chat_status.user_admin
 
