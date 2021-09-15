c0dba2039606eac5128b5295c3144d3a5637b691
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-09-15 20:57:35 +0530

    Update locks.py

--- TGNRobot/modules/locks.py
+++ TGNRobot/modules/locks.py
@@ -9,20 +9,20 @@ from telegram.utils.helpers import mention_html
 
 from alphabet_detector import AlphabetDetector
 
-import LaylaRobot.modules.sql.locks_sql as sql
-from LaylaRobot import dispatcher, DRAGONS, LOGGER
-from LaylaRobot.modules.disable import DisableAbleCommandHandler
-from LaylaRobot.modules.helper_funcs.chat_status import (
+import TGNRobot.modules.sql.locks_sql as sql
+from TGNRobot import dispatcher, DRAGONS, LOGGER
+from TGNRobot.modules.disable import DisableAbleCommandHandler
+from TGNRobot.modules.helper_funcs.chat_status import (
     can_delete,
     is_user_admin,
     user_not_admin,
     is_bot_admin,
     user_admin,
 )
-from LaylaRobot.modules.log_channel import loggable
-from LaylaRobot.modules.connection import connected
-from LaylaRobot.modules.sql.approve_sql import is_approved
-from LaylaRobot.modules.helper_funcs.alternate import send_message, typing_action
+from TGNRobot.modules.log_channel import loggable
+from TGNRobot.modules.connection import connected
+from TGNRobot.modules.sql.approve_sql import is_approved
+from TGNRobot.modules.helper_funcs.alternate import send_message, typing_action
 
 ad = AlphabetDetector()
 
