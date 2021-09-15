e0eddd88e0083c9a1b1fd92eb99e483f425c55fd
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-09-15 21:06:36 +0530

    Update music.py

--- TGNRobot/modules/music.py
+++ TGNRobot/modules/music.py
@@ -12,9 +12,9 @@ from tswift import Song
 from youtube_dl import YoutubeDL
 from youtubesearchpython import SearchVideos
 
-from LaylaRobot.conf import get_str_key
-from LaylaRobot.pyrogramee.pluginshelper import get_text, progress
-from LaylaRobot import pbot
+from TGNRobot.conf import get_str_key
+from TGNRobot.pyrogramee.pluginshelper import get_text, progress
+from TGNRobot import pbot
 
 GENIUS = get_str_key("GENIUS_API_TOKEN", None)
 
