debb556e2d1dabfd3bf51c1e71bca1c8c8d80cb4
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-09-15 20:18:04 +0530

    Update forceSubscribe_sql.py

--- TGNRobot/modules/sql_extended/forceSubscribe_sql.py
+++ TGNRobot/modules/sql_extended/forceSubscribe_sql.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, String, Numeric, Boolean
-from LaylaRobot.modules.sql import BASE, SESSION
+from TGNRobot.modules.sql import BASE, SESSION
 
 
 class forceSubscribe(BASE):
