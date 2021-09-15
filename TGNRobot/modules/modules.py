b7d3c77364fe50fd671429cfde38a64b44faec35
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-09-15 21:42:24 +0530

    Update modules.py

--- TGNRobot/modules/modules.py
+++ TGNRobot/modules/modules.py
@@ -28,7 +28,7 @@ def load(update: Update, context: CallbackContext):
     )
 
     try:
-        imported_module = importlib.import_module("LaylaRobot.modules." + text)
+        imported_module = importlib.import_module("TGNRobot.modules." + text)
     except:
         load_messasge.edit_text("Does that module even exist?")
         return
