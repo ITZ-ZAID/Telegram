987104dd2882032c7f2d784b3ffdbc1d12bba2f6
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-09-15 21:16:19 +0530

    Update truth_and_dare.py

--- TGNRobot/modules/truth_and_dare.py
+++ TGNRobot/modules/truth_and_dare.py
@@ -1,9 +1,9 @@
 import html
 import random
-import LaylaRobot.modules.truth_and_dare_string as truth_and_dare_string
-from LaylaRobot import dispatcher
+import TGNRobot.modules.truth_and_dare_string as truth_and_dare_string
+from TGNRobot import dispatcher
 from telegram import ParseMode, Update, Bot
-from LaylaRobot.modules.disable import DisableAbleCommandHandler
+from TGNRobot.modules.disable import DisableAbleCommandHandler
 from telegram.ext import CallbackContext, run_async
 
 @run_async
