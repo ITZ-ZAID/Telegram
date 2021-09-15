3e172d1ffd00d67350d74575e44d829afca5c8d0
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-09-15 21:44:57 +0530

    Update log_channel.py

--- TGNRobot/modules/log_channel.py
+++ TGNRobot/modules/log_channel.py
@@ -13,9 +13,9 @@ if is_module_loaded(FILENAME):
     from telegram.ext import CommandHandler, JobQueue, run_async
     from telegram.utils.helpers import escape_markdown
 
-    from LaylaRobot import EVENT_LOGS, LOGGER, dispatcher
-    from LaylaRobot.modules.helper_funcs.chat_status import user_admin
-    from LaylaRobot.modules.sql import log_channel_sql as sql
+    from TGNRobot import EVENT_LOGS, LOGGER, dispatcher
+    from TGNRobot.modules.helper_funcs.chat_status import user_admin
+    from TGNRobot.modules.sql import log_channel_sql as sql
 
     def loggable(func):
         @wraps(func)
