00a40ed7811f62028d50b5c293e3282812f6aedb
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-09-15 20:16:49 +0530

    Update welcome_sql.py

--- TGNRobot/modules/sql/welcome_sql.py
+++ TGNRobot/modules/sql/welcome_sql.py
@@ -2,8 +2,8 @@ import random
 import threading
 from typing import Union
 
-from LaylaRobot.modules.helper_funcs.msg_types import Types
-from LaylaRobot.modules.sql import BASE, SESSION
+from TGNRobot.modules.helper_funcs.msg_types import Types
+from TGNRobot.modules.sql import BASE, SESSION
 from sqlalchemy import BigInteger, Boolean, Column, Integer, String, UnicodeText
 
 DEFAULT_WELCOME = "Hey {first}, how are you?"
