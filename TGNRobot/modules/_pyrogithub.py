8d3b4c5f69dc80ce4126df6ffe2daa6f2ecf3209
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-09-15 20:34:11 +0530

    Update _pyrogithub.py

--- TGNRobot/modules/_pyrogithub.py
+++ TGNRobot/modules/_pyrogithub.py
@@ -1,8 +1,8 @@
 # © @Mr_Dark_Prince
 import aiohttp
 from pyrogram import filters
-from LaylaRobot import pbot
-from LaylaRobot.pyrogramee.errors import capture_err
+from TGNRobot import pbot
+from TGNRobot.pyrogramee.errors import capture_err
 
 
 __mod_name__ = "Github"
