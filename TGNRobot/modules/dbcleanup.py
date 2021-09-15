d39d8cd9ca643aea94b8060df4a68af210900851
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-09-15 20:47:39 +0530

    Update dbcleanup.py

--- TGNRobot/modules/dbcleanup.py
+++ TGNRobot/modules/dbcleanup.py
@@ -1,9 +1,9 @@
 from time import sleep
 
-import LaylaRobot.modules.sql.global_bans_sql as gban_sql
-import LaylaRobot.modules.sql.users_sql as user_sql
-from LaylaRobot import DEV_USERS, OWNER_ID, dispatcher
-from LaylaRobot.modules.helper_funcs.chat_status import dev_plus
+import TGNRobot.modules.sql.global_bans_sql as gban_sql
+import TGNRobot.modules.sql.users_sql as user_sql
+from TGNRobot import DEV_USERS, OWNER_ID, dispatcher
+from TGNRobot.modules.helper_funcs.chat_status import dev_plus
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.error import BadRequest, Unauthorized
 from telegram.ext import (
