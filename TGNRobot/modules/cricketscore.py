a92dd48ae83024191253ca5564cceac99591c123
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-09-15 20:45:35 +0530

    Update cricketscore.py

--- TGNRobot/modules/cricketscore.py
+++ TGNRobot/modules/cricketscore.py
@@ -2,7 +2,7 @@ import urllib.request
 
 from bs4 import BeautifulSoup
 from telethon import events
-from LaylaRobot import telethn as tbot
+from TGNRobot import telethn as tbot
 from telethon.tl import functions, types
 from telethon.tl.types import *
 
