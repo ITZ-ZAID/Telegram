da55b1b1405b6dd58895199380492090be1b457b
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-09-15 20:57:57 +0530

    Update log_channel.py

--- TGNRobot/modules/log_channel.py
+++ TGNRobot/modules/log_channel.py
@@ -3,7 +3,7 @@ from functools import wraps
 
 from telegram.ext import CallbackContext
 
-from LaylaRobot.modules.helper_funcs.misc import is_module_loaded
+from TGNRobot.modules.helper_funcs.misc import is_module_loaded
 
 FILENAME = __name__.rsplit(".", 1)[-1]
 
