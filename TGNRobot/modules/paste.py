921a9e5bd0a23996e87bc441617b01372c2ad17b
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-09-15 21:11:02 +0530

    Update paste.py

--- TGNRobot/modules/paste.py
+++ TGNRobot/modules/paste.py
@@ -1,6 +1,6 @@
 import requests
-from LaylaRobot import dispatcher
-from LaylaRobot.modules.disable import DisableAbleCommandHandler
+from TGNRobot import dispatcher
+from TGNRobot.modules.disable import DisableAbleCommandHandler
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 
