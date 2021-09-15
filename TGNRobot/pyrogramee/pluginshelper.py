fd2c4b02d1cd1473af043c29d6514b7e8369550d
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-09-15 20:01:57 +0530

    Update pluginshelper.py

--- TGNRobot/pyrogramee/pluginshelper.py
+++ TGNRobot/pyrogramee/pluginshelper.py
@@ -12,8 +12,8 @@ from pyrogram import Client
 from pyrogram.errors import FloodWait, MessageNotModified
 from pyrogram.types import Chat, Message, User
 
-from LaylaRobot  import OWNER_ID, SUPPORT_CHAT
-from LaylaRobot import pbot
+from TGNRobot  import OWNER_ID, SUPPORT_CHAT
+from TGNRobot import pbot
 
 
 def get_user(message: Message, text: str) -> [int, str, None]:
