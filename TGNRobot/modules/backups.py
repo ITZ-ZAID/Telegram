be98b800851479bcba51438037a3d90cb9377306
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-09-15 22:22:04 +0530

    Update backups.py

--- TGNRobot/modules/backups.py
+++ TGNRobot/modules/backups.py
@@ -341,7 +341,7 @@ def export_data(update, context):
         pass
     context.bot.sendDocument(
         current_chat_id,
-        document=open("LaylaRobot{}.backup".format(chat_id), "rb"),
+        document=open("TGNRobot{}.backup".format(chat_id), "rb"),
         caption="💾*Successfully Exported backup:*\nChat: `{}`\nChat ID: `{}`\nOn: `{}`\n\nNote: This `LaylaRobot-Backup` was specially made for notes 📚.".format(
             chat.title, chat_id, tgl
         ),
