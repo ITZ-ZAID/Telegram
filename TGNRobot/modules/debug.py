862edaf5036aa88dddb517e5d6323620f650e174
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-09-15 20:48:02 +0530

    Update debug.py

--- TGNRobot/modules/debug.py
+++ TGNRobot/modules/debug.py
@@ -5,8 +5,8 @@ from telethon import events
 from telegram import Update
 from telegram.ext import CallbackContext, CommandHandler, run_async
 
-from LaylaRobot import telethn, dispatcher
-from LaylaRobot.modules.helper_funcs.chat_status import dev_plus
+from TGNRobot import telethn, dispatcher
+from TGNRobot.modules.helper_funcs.chat_status import dev_plus
 
 DEBUG_MODE = False
 
