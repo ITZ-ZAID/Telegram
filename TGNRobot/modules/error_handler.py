0850af85b853130a2b67d7e7b5a7f8a127a30dd5
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-09-15 20:50:38 +0530

    Update error_handler.py

--- TGNRobot/modules/error_handler.py
+++ TGNRobot/modules/error_handler.py
@@ -9,7 +9,7 @@ import pretty_errors
 import io
 from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.ext import CallbackContext, CommandHandler
-from LaylaRobot import dispatcher, DEV_USERS, OWNER_ID
+from TGNRobot import dispatcher, DEV_USERS, OWNER_ID
 
 pretty_errors.mono()
 
