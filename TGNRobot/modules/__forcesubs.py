1775ff6c8d42f78cb960faf26c8057b4649c23b4
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-09-15 20:22:29 +0530

    Update __forcesubs.py

--- TGNRobot/modules/__forcesubs.py
+++ TGNRobot/modules/__forcesubs.py
@@ -12,9 +12,9 @@ from pyrogram.errors.exceptions.bad_request_400 import (
 )
 from pyrogram.types import ChatPermissions, InlineKeyboardButton, InlineKeyboardMarkup
 
-from LaylaRobot import DRAGONS as SUDO_USERS
-from LaylaRobot import pbot
-from LaylaRobot.modules.sql_extended import forceSubscribe_sql as sql
+from TGNRobot import DRAGONS as SUDO_USERS
+from TGNRobot import pbot
+from TGNRobot.modules.sql_extended import forceSubscribe_sql as sql
 
 logging.basicConfig(level=logging.INFO)
 
