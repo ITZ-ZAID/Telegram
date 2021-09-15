109499b5a697097b4c58f5762fc6d81ec7f7f5ad
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-09-15 20:09:53 +0530

    Update cust_filters_sql.py

--- TGNRobot/modules/sql/cust_filters_sql.py
+++ TGNRobot/modules/sql/cust_filters_sql.py
@@ -2,8 +2,8 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, Boolean, Integer, distinct, func
 
-from LaylaRobot.modules.helper_funcs.msg_types import Types
-from LaylaRobot.modules.sql import BASE, SESSION
+from TGNRobot.modules.helper_funcs.msg_types import Types
+from TGNRobot.modules.sql import BASE, SESSION
 
 
 class CustomFilters(BASE):
