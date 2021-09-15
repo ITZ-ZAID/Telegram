19096ec5f0bbf9c42b40ea84d4093e3295ec1366
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-09-15 20:21:28 +0530

    Update __country.py

--- TGNRobot/modules/__country.py
+++ TGNRobot/modules/__country.py
@@ -5,8 +5,8 @@ from telethon.utils import get_input_location
 import flag
 import html, os
 from countryinfo import CountryInfo
-from LaylaRobot import telethn as borg
-from LaylaRobot.events import register
+from TGNRobot import telethn as borg
+from TGNRobot.events import register
 
 
 @register(pattern="^/country (.*)")
