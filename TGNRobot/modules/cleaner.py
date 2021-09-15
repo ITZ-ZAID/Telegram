c94b89e72f059d4013b17dd7413f474bf83ea211
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-09-15 20:44:30 +0530

    Update cleaner.py

--- TGNRobot/modules/cleaner.py
+++ TGNRobot/modules/cleaner.py
@@ -1,15 +1,15 @@
 
 import html
 
-from LaylaRobot import ALLOW_EXCL, CustomCommandHandler, dispatcher
-from LaylaRobot.modules.disable import DisableAbleCommandHandler
-from LaylaRobot.modules.helper_funcs.chat_status import (
+from TGNRobot import ALLOW_EXCL, CustomCommandHandler, dispatcher
+from TGNRobot.modules.disable import DisableAbleCommandHandler
+from TGNRobot.modules.helper_funcs.chat_status import (
     bot_can_delete,
     connection_status,
     dev_plus,
     user_admin,
 )
-from LaylaRobot.modules.sql import cleaner_sql as sql
+from TGNRobot.modules.sql import cleaner_sql as sql
 from telegram import ParseMode, Update
 from telegram.ext import (
     CallbackContext,
