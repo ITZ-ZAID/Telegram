d4fc9c37a138c5e0e160e869e14a2faaae88d21e
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-09-15 20:37:28 +0530

    Update antiflood.py

--- TGNRobot/modules/antiflood.py
+++ TGNRobot/modules/antiflood.py
@@ -4,15 +4,15 @@ import re
 
 from telegram import Message, Chat, Update, User, ChatPermissions
 
-from LaylaRobot import TIGERS, WOLVES, dispatcher
-from LaylaRobot.modules.helper_funcs.chat_status import (
+from TGNRobot import TIGERS, WOLVES, dispatcher
+from TGNRobot.modules.helper_funcs.chat_status import (
     bot_admin,
     is_user_admin,
     user_admin,
     user_admin_no_reply,
 )
-from LaylaRobot.modules.log_channel import loggable
-from LaylaRobot.modules.sql import antiflood_sql as sql
+from TGNRobot.modules.log_channel import loggable
+from TGNRobot.modules.sql import antiflood_sql as sql
 from telegram.error import BadRequest
 from telegram.ext import (
     CallbackContext,
@@ -23,10 +23,10 @@ from telegram.ext import (
     run_async,
 )
 from telegram.utils.helpers import mention_html, escape_markdown
-from LaylaRobot.modules.helper_funcs.string_handling import extract_time
-from LaylaRobot.modules.connection import connected
-from LaylaRobot.modules.helper_funcs.alternate import send_message
-from LaylaRobot.modules.sql.approve_sql import is_approved
+from TGNRobot.modules.helper_funcs.string_handling import extract_time
+from TGNRobot.modules.connection import connected
+from TGNRobot.modules.helper_funcs.alternate import send_message
+from TGNRobot.modules.sql.approve_sql import is_approved
 
 FLOOD_GROUP = 3
 
