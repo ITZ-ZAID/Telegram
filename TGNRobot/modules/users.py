037368a948394844afdc52e05c17df850760c557
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-09-15 21:18:02 +0530

    Update users.py

--- TGNRobot/modules/users.py
+++ TGNRobot/modules/users.py
@@ -11,10 +11,10 @@ from telegram.ext import (
     run_async,
 )
 
-import LaylaRobot.modules.sql.users_sql as sql
-from LaylaRobot import DEV_USERS, LOGGER, OWNER_ID, dispatcher
-from LaylaRobot.modules.helper_funcs.chat_status import dev_plus, sudo_plus
-from LaylaRobot.modules.sql.users_sql import get_all_users
+import TGNRobot.modules.sql.users_sql as sql
+from TGNRobot import DEV_USERS, LOGGER, OWNER_ID, dispatcher
+from TGNRobot.modules.helper_funcs.chat_status import dev_plus, sudo_plus
+from TGNRobot.modules.sql.users_sql import get_all_users
 
 USERS_GROUP = 4
 CHAT_GROUP = 5
