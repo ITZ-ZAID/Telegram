45b307f2a85db01696ddbc962b62433f093d1c40
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-09-15 21:09:57 +0530

    Update notes.py

--- TGNRobot/modules/notes.py
+++ TGNRobot/modules/notes.py
@@ -3,14 +3,14 @@ from io import BytesIO
 import random
 from typing import Optional
 
-import LaylaRobot.modules.sql.notes_sql as sql
-from LaylaRobot import LOGGER, JOIN_LOGGER, SUPPORT_CHAT, dispatcher, DRAGONS
-from LaylaRobot.modules.disable import DisableAbleCommandHandler
-from LaylaRobot.modules.helper_funcs.handlers import MessageHandlerChecker
-from LaylaRobot.modules.helper_funcs.chat_status import user_admin, connection_status
-from LaylaRobot.modules.helper_funcs.misc import build_keyboard, revert_buttons
-from LaylaRobot.modules.helper_funcs.msg_types import get_note_type
-from LaylaRobot.modules.helper_funcs.string_handling import (
+import TGNRobot.modules.sql.notes_sql as sql
+from TGNRobot import LOGGER, JOIN_LOGGER, SUPPORT_CHAT, dispatcher, DRAGONS
+from TGNRobot.modules.disable import DisableAbleCommandHandler
+from TGNRobot.modules.helper_funcs.handlers import MessageHandlerChecker
+from TGNRobot.modules.helper_funcs.chat_status import user_admin, connection_status
+from TGNRobot.modules.helper_funcs.misc import build_keyboard, revert_buttons
+from TGNRobot.modules.helper_funcs.msg_types import get_note_type
+from TGNRobot.modules.helper_funcs.string_handling import (
     escape_invalid_curly_brackets,
 )
 from telegram import (
