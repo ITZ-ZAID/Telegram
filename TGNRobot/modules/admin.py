c69a4539bfd0cf36a744bc72a43c3bd2d2df1ef1
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-09-15 20:35:21 +0530

    Update admin.py

--- TGNRobot/modules/admin.py
+++ TGNRobot/modules/admin.py
@@ -5,9 +5,9 @@ from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, Filters, run_async
 from telegram.utils.helpers import mention_html
 
-from LaylaRobot import DRAGONS, dispatcher
-from LaylaRobot.modules.disable import DisableAbleCommandHandler
-from LaylaRobot.modules.helper_funcs.chat_status import (
+from TGNRobot import DRAGONS, dispatcher
+from TGNRobot.modules.disable import DisableAbleCommandHandler
+from TGNRobot.modules.helper_funcs.chat_status import (
     bot_admin,
     can_pin,
     can_promote,
@@ -15,19 +15,19 @@ from LaylaRobot.modules.helper_funcs.chat_status import (
     user_admin,
     ADMIN_CACHE,
 )
-from LaylaRobot.helper_extra.admin_rights import (
+from TGNRobot.helper_extra.admin_rights import (
     user_can_pin,
     user_can_promote,
     user_can_changeinfo,
 )
 
-from LaylaRobot.modules.helper_funcs.extraction import (
+from TGNRobot.modules.helper_funcs.extraction import (
     extract_user,
     extract_user_and_text,
 )
-from LaylaRobot.modules.log_channel import loggable
-from LaylaRobot.modules.helper_funcs.alternate import send_message
-from LaylaRobot.modules.helper_funcs.alternate import typing_action
+from TGNRobot.modules.log_channel import loggable
+from TGNRobot.modules.helper_funcs.alternate import send_message
+from TGNRobot.modules.helper_funcs.alternate import typing_action
 
 
 @run_async
