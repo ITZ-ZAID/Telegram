1aad03b72e2fbd718c5d5cdc6f2259eae5b1109f
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-09-15 20:45:58 +0530

    Update currency_converter.py

--- TGNRobot/modules/currency_converter.py
+++ TGNRobot/modules/currency_converter.py
@@ -1,5 +1,5 @@
 import requests
-from LaylaRobot import CASH_API_KEY, dispatcher
+from TGNRobot import CASH_API_KEY, dispatcher
 from telegram import Update, ParseMode
 from telegram.ext import CallbackContext, CommandHandler, run_async
 
