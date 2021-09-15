b26a4aeb88a7889d826fbb0932fcb04827d5acc4
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-09-15 21:06:00 +0530

    Update modules.py

--- TGNRobot/modules/modules.py
+++ TGNRobot/modules/modules.py
@@ -1,8 +1,8 @@
 import importlib
 import collections
 
-from LaylaRobot import dispatcher, telethn
-from LaylaRobot.__main__ import (
+from TGNRobot import dispatcher, telethn
+from TGNRobot.__main__ import (
     CHAT_SETTINGS,
     DATA_EXPORT,
     DATA_IMPORT,
@@ -13,7 +13,7 @@ from LaylaRobot.__main__ import (
     USER_INFO,
     USER_SETTINGS,
 )
-from LaylaRobot.modules.helper_funcs.chat_status import dev_plus, sudo_plus
+from TGNRobot.modules.helper_funcs.chat_status import dev_plus, sudo_plus
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, CommandHandler, run_async
 
