abd3c11da0248308ce4ecb69012d42c38b25c19e
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-09-15 19:52:33 +0530

    Update mongo.py

--- TGNRobot/mongo.py
+++ TGNRobot/mongo.py
@@ -2,10 +2,10 @@ import asyncio
 import sys
 
 from motor import motor_asyncio
-from LaylaRobot import MONGO_DB_URI 
+from TGNRobot import MONGO_DB_URI 
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
-from LaylaRobot.conf import get_int_key, get_str_key
+from TGNRobot.conf import get_int_key, get_str_key
 
 
 MONGO_PORT = get_int_key("27017")
@@ -17,7 +17,7 @@ client = MongoClient()
 client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
 motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
 db = motor[MONGO_DB]
-db = client["LaylaRobot"]
+db = client["TGNRobot"]
 try:
     asyncio.get_event_loop().run_until_complete(motor.server_info())
 except ServerSelectionTimeoutError:
