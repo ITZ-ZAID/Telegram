cd724f6637b2da784763b9f5b2545abc78ac0935
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-09-15 22:17:16 +0530

    Update msg_types.py

--- TGNRobot/modules/helper_funcs/msg_types.py
+++ TGNRobot/modules/helper_funcs/msg_types.py
@@ -1,6 +1,6 @@
 from enum import IntEnum, unique
 
-from LaylaRobot.modules.helper_funcs.string_handling import button_markdown_parser
+from TGNRobot.modules.helper_funcs.string_handling import button_markdown_parser
 from telegram import Message
 
 
