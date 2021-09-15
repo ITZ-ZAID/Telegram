54d2cf87fc00d199a4f6d8b721ad5d6baa682221
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-09-15 21:13:08 +0530

    Update remote_cmds.py

--- TGNRobot/modules/remote_cmds.py
+++ TGNRobot/modules/remote_cmds.py
@@ -1,12 +1,12 @@
-from LaylaRobot import dispatcher
-from LaylaRobot.modules.helper_funcs.chat_status import (
+from TGNRobot import dispatcher
+from TGNRobot.modules.helper_funcs.chat_status import (
     bot_admin,
     is_bot_admin,
     is_user_ban_protected,
     is_user_in_chat,
 )
-from LaylaRobot.modules.helper_funcs.extraction import extract_user_and_text
-from LaylaRobot.modules.helper_funcs.filters import CustomFilters
+from TGNRobot.modules.helper_funcs.extraction import extract_user_and_text
+from TGNRobot.modules.helper_funcs.filters import CustomFilters
 from telegram import Update, ChatPermissions
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, run_async
