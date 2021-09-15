61cc8fe830dee2e770066527bd47b342f010d5b6
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-09-15 20:35:54 +0530

    Update afk.py

--- TGNRobot/modules/afk.py
+++ TGNRobot/modules/afk.py
@@ -1,12 +1,12 @@
 import random, html
 
-from LaylaRobot import dispatcher
-from LaylaRobot.modules.disable import (
+from TGNRobot import dispatcher
+from TGNRobot.modules.disable import (
     DisableAbleCommandHandler,
     DisableAbleMessageHandler,
 )
-from LaylaRobot.modules.sql import afk_sql as sql
-from LaylaRobot.modules.users import get_user_id
+from TGNRobot.modules.sql import afk_sql as sql
+from TGNRobot.modules.users import get_user_id
 from telegram import MessageEntity, Update
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, Filters, MessageHandler, run_async
