34528b9373cda1c82b5d861d3cb629408878f443
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-09-15 20:05:10 +0530

    Update extraction.py

--- TGNRobot/modules/helper_funcs/extraction.py
+++ TGNRobot/modules/helper_funcs/extraction.py
@@ -1,7 +1,7 @@
 from typing import List, Optional
 
-from LaylaRobot import LOGGER
-from LaylaRobot.modules.users import get_user_id
+from TGNRobot import LOGGER
+from TGNRobot.modules.users import get_user_id
 from telegram import Message, MessageEntity
 from telegram.error import BadRequest
 
