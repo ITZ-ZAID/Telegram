81d6c551c753df1396e464e675d4fba77ae71aa8
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-09-15 21:26:09 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
-TITLE Layla Robot
+TITLE TGN Robot
 :: Enables virtual env mode and then starts Rika
-env\scripts\activate.bat && py -m LaylaRobot
+env\scripts\activate.bat && py -m TGNRobot
