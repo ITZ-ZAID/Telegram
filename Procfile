a25d17bc380bd57c959053b6fc82aecb19978588
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-09-15 19:06:37 +0530

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,2 @@
+worker: python3 -m LaylaRobot
+ps:scale worker=1
---
+++ start.bat
@@ -0,0 +1,4 @@
+@echo off
+TITLE Layla Robot
+:: Enables virtual env mode and then starts Rika
+env\scripts\activate.bat && py -m LaylaRobot
