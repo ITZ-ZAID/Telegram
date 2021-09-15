aa6ac2a801af25bc8d3eeaa0e78e10bfb691c574
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-09-15 21:35:03 +0530

    Update start_service.bat

--- start_service.bat
+++ start_service.bat
@@ -27,5 +27,5 @@ if '%errorlevel%' NEQ '0' (
 :--------------------------------------
 :: your commands begin from this point.
 :: stops the service and then starts it 
-net stop LaylaRobot
-net start LaylaRobot
+net stop TGNRobot
+net start TGNRobot
