a4b2e2f8a170605416c275dbb6066ba126e46beb
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-09-15 22:42:08 +0530

    Update welcome.py

--- TGNRobot/modules/welcome.py
+++ TGNRobot/modules/welcome.py
@@ -265,7 +265,7 @@ def new_member(update: Update, context: CallbackContext):
             # Welcome yourself
             elif new_mem.id == bot.id:
                 creator = None
-                if not LaylaRobot.ALLOW_CHATS:
+                if not TGNRobot.ALLOW_CHATS:
                     with suppress(BadRequest):
                          update.effective_message.reply_text(f"Groups are disabled for {bot.first_name}, I'm outta here.")
                     bot.leave_chat(update.effective_chat.id)
