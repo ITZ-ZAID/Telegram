492be1658f72ee081320ea08353f370359a3a6f0
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-09-15 20:33:09 +0530

    Update __weather.py

--- TGNRobot/modules/__weather.py
+++ TGNRobot/modules/__weather.py
@@ -1,4 +1,4 @@
-from LaylaRobot import telethn as tbot
+from TGNRobot import telethn as tbot
 import io
 import os
 import time
@@ -9,9 +9,9 @@ from telethon.tl import functions
 from telethon.tl import types
 from telethon.tl.types import *
 
-from LaylaRobot import *
+from TGNRobot import *
 
-from LaylaRobot.events import register
+from TGNRobot.events import register
 
 
 async def is_register_admin(chat, user):
