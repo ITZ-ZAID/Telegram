17f6690f91d6bb9b35b9141ab218f4f0b74ad3b3
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-09-15 20:49:31 +0530

    Update disable.py

--- TGNRobot/modules/disable.py
+++ TGNRobot/modules/disable.py
@@ -2,9 +2,9 @@ import importlib
 from typing import Union
 
 from future.utils import string_types
-from LaylaRobot import dispatcher
-from LaylaRobot.modules.helper_funcs.handlers import CMD_STARTERS, SpamChecker
-from LaylaRobot.modules.helper_funcs.misc import is_module_loaded
+from TGNRobot import dispatcher
+from TGNRobot.modules.helper_funcs.handlers import CMD_STARTERS, SpamChecker
+from TGNRobot.modules.helper_funcs.misc import is_module_loaded
 from telegram import ParseMode, Update
 from telegram.ext import (
     CallbackContext,
@@ -20,12 +20,12 @@ FILENAME = __name__.rsplit(".", 1)[-1]
 # If module is due to be loaded, then setup all the magical handlers
 if is_module_loaded(FILENAME):
 
-    from LaylaRobot.modules.helper_funcs.chat_status import (
+    from TGNRobot.modules.helper_funcs.chat_status import (
         connection_status,
         is_user_admin,
         user_admin,
     )
-    from LaylaRobot.modules.sql import disable_sql as sql
+    from TGNRobot.modules.sql import disable_sql as sql
     from telegram.ext.dispatcher import run_async
 
     DISABLE_CMDS = []
