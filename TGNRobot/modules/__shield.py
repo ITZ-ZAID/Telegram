702f9b1ce88216a1ea80bce18b50ffed6a6fb6d2
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-09-15 22:13:39 +0530

    Update __shield.py

--- TGNRobot/modules/__shield.py
+++ TGNRobot/modules/__shield.py
@@ -48,7 +48,7 @@ MONGO_DB_URI = get_str_key("MONGO_DB_URI")
 
 client = MongoClient()
 client = MongoClient(MONGO_DB_URI)
-db = client["LaylaRobot"]
+db = client["TGNRobot"]
 
 async def is_nsfw(event):
     lmao = event
