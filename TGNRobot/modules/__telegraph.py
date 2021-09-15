f7f2a3b138b62ac48c7f96a084890c5910d99a38
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-09-15 20:30:36 +0530

    Update __telegraph.py

--- TGNRobot/modules/__telegraph.py
+++ TGNRobot/modules/__telegraph.py
@@ -8,7 +8,7 @@ from datetime import datetime
 from telegraph import Telegraph, upload_file, exceptions
 Zaid = "tgn"
 telegraph = Telegraph()
-r = telegraph.create_account(short_name=Hero)
+r = telegraph.create_account(short_name=Zaid)
 auth_url = r["auth_url"]
 
 
