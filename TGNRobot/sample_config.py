fa4411950b78b59791ccf6018bba5d10a89975c4
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-09-15 19:51:26 +0530

    Update sample_config.py

--- TGNRobot/sample_config.py
+++ TGNRobot/sample_config.py
@@ -4,7 +4,7 @@ import os
 
 
 def get_user_list(config, key):
-    with open("{}/LaylaRobot/{}".format(os.getcwd(), config), "r") as json_file:
+    with open("{}/TGNRobot/{}".format(os.getcwd(), config), "r") as json_file:
         return json.load(json_file)[key]
 
 
@@ -15,11 +15,11 @@ class Config(object):
     # Login to https://my.telegram.org and fill in these slots with the details given by it
 
     API_ID = 123456  # integer value, dont use ""
-    API_HASH = "awoo"
+    API_HASH = "boom"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 797768146  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "HEROGAMERS1" 
-    SUPPORT_CHAT = "AwesomeSupport"  # Your own group for support, do not add the @
+    OWNER_ID = 1669178360  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "Timesisnotwaiting" 
+    SUPPORT_CHAT = "GodfatherSupport"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
         -1001392103597
     )  # Prints any new group the bot is added to, prints just the name and ID.
@@ -60,7 +60,7 @@ class Config(object):
     CASH_API_KEY = (
         "awoo"  # Get your API key from https://www.alphavantage.co/support/#api-key
     )
-    TIME_API_KEY = "awoo"  # Get your API key from https://timezonedb.com/api
+    TIME_API_KEY = "zaid"  # Get your API key from https://timezonedb.com/api
     WALL_API = (
         "awoo"  # For wallpapers, get one from https://wall.alphacoders.com/api.php
     )
