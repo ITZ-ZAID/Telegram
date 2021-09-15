efb74a1e06894bc8ad56867f2a47725230eb48eb
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-09-15 20:06:25 +0530

    Update antiflood_sql.py

--- TGNRobot/modules/sql/antiflood_sql.py
+++ TGNRobot/modules/sql/antiflood_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import String, Column, Integer, UnicodeText
 
-from LaylaRobot.modules.sql import SESSION, BASE
+from TGNRobot.modules.sql import SESSION, BASE
 
 DEF_COUNT = 1
 DEF_LIMIT = 0
