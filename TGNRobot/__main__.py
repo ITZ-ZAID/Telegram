2e4c4c3c0fa52c41df8d198ca2db81973f0db736
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-10-09 13:43:41 +0530

    Update __main__.py

--- TGNRobot/__main__.py
+++ TGNRobot/__main__.py
@@ -224,7 +224,7 @@ def start(update: Update, context: CallbackContext):
             )
     else:
         update.effective_message.reply_video(
-            START_IMG, caption= "<code>Sophia is Here For You❤\nI am Awake Since</code>: <code>{}</code>".format(
+            START_IMG, caption= "<code>TGN is Here For You❤\nI am Awake Since</code>: <code>{}</code>".format(
                 uptime            
             ),
             parse_mode=ParseMode.HTML,
