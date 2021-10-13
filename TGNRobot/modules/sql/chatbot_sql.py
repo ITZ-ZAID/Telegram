056588f20dc30d452031183789d7cf1b91b7a33d
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-10-13 10:37:25 +0530

    Update chatbot_sql.py

--- TGNRobot/modules/sql/chatbot_sql.py
+++ TGNRobot/modules/sql/chatbot_sql.py
@@ -1,73 +1,42 @@
 import threading
-
-from TGNRobot.modules.sql import BASE, SESSION
 from sqlalchemy import Column, String
-
-
-class ChatbotChats(BASE):
-    __tablename__ = "chatbot_chats"
+from TGNRobot.modules.sql import BASE, SESSION
+class KukiChats(BASE):
+    __tablename__ = "kuki_chats"
     chat_id = Column(String(14), primary_key=True)
-    ses_id = Column(String(70))
-    expires = Column(String(15))
 
-    def __init__(self, chat_id, ses_id, expires):
+    def __init__(self, chat_id):
         self.chat_id = chat_id
-        self.ses_id = ses_id
-        self.expires = expires
 
-
-ChatbotChats.__table__.create(checkfirst=True)
-
+KukiChats.__table__.create(checkfirst=True)
 INSERTION_LOCK = threading.RLock()
 
 
-def is_chat(chat_id):
+def is_kuki(chat_id):
     try:
-        chat = SESSION.query(ChatbotChats).get(str(chat_id))
-        if chat:
-            return True
-        else:
-            return False
+        chat = SESSION.query(KukiChats).get(str(chat_id))
+        return bool(chat)
     finally:
         SESSION.close()
 
-
-def set_ses(chat_id, ses_id, expires):
+def set_kuki(chat_id):
     with INSERTION_LOCK:
-        autochat = SESSION.query(ChatbotChats).get(str(chat_id))
-        if not autochat:
-            autochat = ChatbotChats(str(chat_id), str(ses_id), str(expires))
-        else:
-            autochat.ses_id = str(ses_id)
-            autochat.expires = str(expires)
+        kukichat = SESSION.query(KukiChats).get(str(chat_id))
+        if not kukichat:
+            kukichat = KukiChats(str(chat_id))
+        SESSION.add(kukichat)
+        SESSION.commit()
 
-        SESSION.add(autochat)
+def rem_kuki(chat_id):
+    with INSERTION_LOCK:
+        kukichat = SESSION.query(KukiChats).get(str(chat_id))
+        if kukichat:
+            SESSION.delete(kukichat)
         SESSION.commit()
 
 
-def get_ses(chat_id):
-    autochat = SESSION.query(ChatbotChats).get(str(chat_id))
-    sesh = ""
-    exp = ""
-    if autochat:
-        sesh = str(autochat.ses_id)
-        exp = str(autochat.expires)
-
-    SESSION.close()
-    return sesh, exp
-
-
-def rem_chat(chat_id):
-    with INSERTION_LOCK:
-        autochat = SESSION.query(ChatbotChats).get(str(chat_id))
-        if autochat:
-            SESSION.delete(autochat)
-
-        SESSION.commit()
-
-
-def get_all_chats():
+def get_all_kuki_chats():
     try:
-        return SESSION.query(ChatbotChats.chat_id).all()
+        return SESSION.query(KukiChats.chat_id).all()
     finally:
         SESSION.close()
