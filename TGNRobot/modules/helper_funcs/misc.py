afeee87ef410b32528f95c382798dcab21da1706
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-10-19 12:09:39 +0530

    Update misc.py

--- TGNRobot/modules/helper_funcs/misc.py
+++ TGNRobot/modules/helper_funcs/misc.py
@@ -66,12 +66,12 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     # can only have a certain amount of buttons side by side
     if len(pairs) > 8:
         pairs = pairs[modulo_page * 8:8 * (modulo_page + 1)] + [
-            (EqInlineKeyboardButton("ᐊ", callback_data="{}_prev({})".format(prefix, modulo_page)),
-                EqInlineKeyboardButton("Back", callback_data="layla_back"),
-             EqInlineKeyboardButton("ᐅ", callback_data="{}_next({})".format(prefix, modulo_page)))]
+            (EqInlineKeyboardButton("☜", callback_data="{}_prev({})".format(prefix, modulo_page)),
+                EqInlineKeyboardButton("Back", callback_data="zaid_back"),
+             EqInlineKeyboardButton("☞", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     else:
-        pairs += [[EqInlineKeyboardButton("Back", callback_data="layla_back")]]
+        pairs += [[EqInlineKeyboardButton("Back", callback_data="zaid_back")]]
 
     return pairs
 
