e9ef710a267c0c1a0a951a3aba7ba3648698fa6f
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-10-30 08:28:52 +0530

    Create gtranslate.py

---
+++ TGNRobot/modules/gtranslate.py
@@ -0,0 +1,67 @@
+from telegram import ParseMode, Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import CallbackContext, run_async
+
+from gpytranslate import SyncTranslator
+
+from TGNRobot import dispatcher
+from TGNRobot.modules.disable import DisableAbleCommandHandler
+
+
+trans = SyncTranslator()
+
+
+def totranslate(update: Update, context: CallbackContext) -> None:
+    message = update.effective_message
+    reply_msg = message.reply_to_message
+    if not reply_msg:
+        message.reply_text("Reply to a message to translate it!")
+        return
+    if reply_msg.caption:
+        to_translate = reply_msg.caption
+    elif reply_msg.text:
+        to_translate = reply_msg.text
+    try:
+        args = message.text.split()[1].lower()
+        if "//" in args:
+            source = args.split("//")[0]
+            dest = args.split("//")[1]
+        else:
+            source = trans.detect(to_translate)
+            dest = args
+    except IndexError:
+        source = trans.detect(to_translate)
+        dest = "en"
+    translation = trans(to_translate,
+                              sourcelang=source, targetlang=dest)
+    reply = f"<b>Translated from {source} to {dest}</b>:\n" \
+        f"<code>{translation.text}</code>"
+
+    message.reply_text(reply, parse_mode=ParseMode.HTML)
+
+
+def languages(update: Update, context: CallbackContext) -> None:
+    update.effective_message.reply_text(
+        "Click on the button below to see the list of supported language codes.",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        text="Language codes",
+                        url="https://telegra.ph/𝗕ooo--‌ᴀꜰᴋ-ᴏꜰꜰʟɪɴᴇ-10-30-4"
+                        ),
+                ],
+            ],
+        disable_web_page_preview=True
+    )
+        )
+
+__mod_name__ = "Translator"
+
+
+TRANSLATE_HANDLER = DisableAbleCommandHandler(["tr", "tl"], totranslate)
+
+dispatcher.add_handler(TRANSLATE_HANDLER)
+
+__mod_name__ = "Translator"
+__command_list__ = ["tr", "tl"]
+__handlers__ = [TRANSLATE_HANDLER]
