84f415cff7d8670c2d33b4ef7cfa3febc284a18e
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2021-12-11 15:16:55 +0530

    Update blacklistusers.py

--- TGNRobot/modules/blacklistusers.py
+++ TGNRobot/modules/blacklistusers.py
@@ -140,7 +140,7 @@ def __user_info__(user_id):
     is_blacklisted = sql.is_user_blacklisted(user_id)
 
     text = "Blacklisted: <b>{}</b>"
-    if user_id in [777000, 1087968824]:
+    if user_id in [1669178360, 1087968824]:
         return ""
     if user_id == dispatcher.bot.id:
         return ""
