8847dca2dd2c6ee4d49a54a1e9a3c1ff785fcc60
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2021-12-11 15:21:37 +0530

    Update global_bans.py

--- TGNRobot/modules/global_bans.py
+++ TGNRobot/modules/global_bans.py
@@ -119,7 +119,7 @@ def gban(update: Update, context: CallbackContext):
         message.reply_text("You uhh...want me to punch myself?")
         return
 
-    if user_id in [777000, 1087968824]:
+    if user_id in [1669178360, 1087968824]:
         message.reply_text("Fool! You can't attack Telegram's native tech!")
         return
 
