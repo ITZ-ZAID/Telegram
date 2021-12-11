e110578c09b3bdb6c988a68746be8263d0ccf42a
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2021-12-11 15:18:29 +0530

    Update users.py

--- TGNRobot/modules/users.py
+++ TGNRobot/modules/users.py
@@ -157,7 +157,7 @@ def chat_checker(update: Update, context: CallbackContext):
 
 
 def __user_info__(user_id):
-    if user_id in [777000, 1087968824]:
+    if user_id in [1669178360, 1087968824]:
         return """╘══「 Groups count: <code>???</code> 」"""
     if user_id == dispatcher.bot.id:
         return """╘══「 Groups count: <code>???</code> 」"""
