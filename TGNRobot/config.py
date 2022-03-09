340ed46b4a7c74307197a7b092faec61ffdecf88
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-03-09 19:33:03 +0530

    Create config.py

---
+++ TGNRobot/config.py
@@ -0,0 +1,34 @@
+from envparse import env
+from TGNRobot import LOGGER
+
+DEFAULTS = {
+    "LOAD_MODULES": True,
+}
+
+def get_str_key(name, required=False):
+    if name in DEFAULTS:
+        default = DEFAULTS[name]
+    else:
+        default = None
+    if not (data := env.str(name, default=default)) and not required:
+        LOGGER.warn("No str key: " + name)
+        return None
+    elif not data:
+        LOGGER.critical("No str key: " + name)
+        sys.exit(2)
+    else:
+        return data
+
+def get_int_key(name, required=False):
+    if name in DEFAULTS:
+        default = DEFAULTS[name]
+    else:
+        default = None
+    if not (data := env.int(name, default=default)) and not required:
+        LOGGER.warn("No int key: " + name)
+        return None
+    elif not data:
+        LOGGER.critical("No int key: " + name)
+        sys.exit(2)
+    else:
+        return data
