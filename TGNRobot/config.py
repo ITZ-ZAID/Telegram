801432a0561e5d0335e898ef367cf29fc2cb98ea
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-03-09 19:47:49 +0530

    Update config.py

--- TGNRobot/config.py
+++ TGNRobot/config.py
@@ -77,6 +77,7 @@ class Config(object):
     AI_API_KEY = "awoo"  # For chatbot, get one from https://coffeehouse.intellivoid.net/dashboard
     BL_CHATS = []  # List of groups that you want blacklisted.
     SPAMMERS = None
+    REDIS_URL = None
 
 
 class Production(Config):
