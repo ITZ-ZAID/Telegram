dd079c59b42613ef6b387e98258645329515aeb7
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-04-08 09:38:53 +0530

    Add files via upload

---
+++ Telegram/langs/__init__.py
@@ -0,0 +1,6 @@
+from Telegram.langs.language import langs
+
+get_string = langs.get_string
+reload_strings = langs.reload_strings
+get_languages = langs.get_languages
+get_language = langs.get_language
