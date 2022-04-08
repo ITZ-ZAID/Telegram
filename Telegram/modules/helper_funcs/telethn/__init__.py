7c07fe24a2499a5fd9bbf015cfec8feeb5bd8169
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-04-08 09:50:56 +0530

    Telethon clients

---
+++ Telegram/modules/helper_funcs/telethn/__init__.py
@@ -0,0 +1,14 @@
+from Telegram import (
+    telethn,
+    SUDO_USERS,
+    WHITELIST_USERS,
+    SUPPORT_USERS,
+    SARDEGNA_USERS,
+    DEV_USERS,
+)
+
+HIGHER_AUTH = SUDO_USERS + DEV_USERS
+
+HIGHER_AUTH = list(SUDO_USERS) + list(DEV_USERS)
+
+HIGHER_AUTH.append(1087968824)
