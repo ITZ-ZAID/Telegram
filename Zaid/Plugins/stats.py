b71ae7ca1516f40d32de80c09618834f36a69db7
Zaid <itzzaid@pm.me>
2022-09-22 19:04:36 +0530

    Update stats.py

--- Zaid/Plugins/stats.py
+++ Zaid/Plugins/stats.py
@@ -1,5 +1,10 @@
+import sys
+import os
+import asyncio
+import io
 from ..utils import Zbot
 from Zaid import Zaid
+from config import OWNER_ID
 from Zaid.Plugins.mongodb.chats_db import get_total_chats
 from Zaid.Plugins.mongodb.notes_db import get_total_notes
 from Zaid.Plugins.mongodb.filters_db import get_total_filters
@@ -8,7 +13,7 @@ from Zaid.Plugins.mongodb.welcome_db import get_total_welcome
 from Zaid.Plugins.mongodb.nightmode_db import get_total_nightmode
 from Zaid.Plugins.mongodb.locks_db import get_total_locks
 
-@Zbot(pattern="^/stats ?(.*)")
+@Zbot(pattern="^/stats ?(.*)", from_users=[OWNER_ID])
 async def stats(event):
     a = get_total_notes()
     b = len(Zaid.list_event_handlers())
@@ -19,3 +24,11 @@ async def stats(event):
     g = get_total_nightmode()
     h = get_total_locks()
     await event.reply(f"✘ Current Stats\n‣ Total Notes: {a}\n‣ Total Commands: {b}\n‣ Total Chats: {c}\n‣ Total Filters: {d}\n‣ Welcome: {e}\n‣ Total Rules: {f}\n‣Total Nightmode: {g}\n‣ Total Locks: {h}")
+
+
+
+@Zbot(pattern="^/restart$", from_users=[OWNER_ID])
+async def restart(e):
+    await e.reply("**__Restarting....__**")
+    args = [sys.executable, "-m", "Zaid"]
+    os.execle(sys.executable, *args, os.environ)
