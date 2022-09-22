a38dcad315c79a6b403d41ad1d47e6a061df3bf4
Zaid <itzzaid@pm.me>
2022-09-22 22:04:19 +0530

    Update notes.py

--- Zaid/Plugins/notes.py
+++ Zaid/Plugins/notes.py
@@ -355,3 +355,33 @@ async def rr(event):
         OUT_STR += f"- [{a_note}](t.me/Zaid2_Robot?start=notes_{luv})\n"
     OUT_STR += "You can retrieve these notes by tapping on the notename."
     await event.reply(OUT_STR, link_preview=False)
+
+
+
+__help__ = """
+ ❍ /get <notename>*:* get the note with this notename
+ ❍ #<notename>*:* same as /get
+ ❍ /notes or /saved*:* list all saved notes in this chat
+ ❍ /number *:* Will pull the note of that number in the list
+If you would like to retrieve the contents of a note without any formatting, use `/get <notename> noformat`. This can \
+be useful when updating a current note
+*Admins only:*
+ ❍ /save <notename> <notedata>*:* saves notedata as a note with name notename
+A button can be added to a note by using standard markdown link syntax - the link should just be prepended with a \
+`buttonurl:` section, as such: `[somelink](buttonurl:example.com)`. Check `/markdownhelp` for more info
+ ❍ /save <notename>*:* save the replied message as a note with name notename
+ Separate diff replies by `%%%` to get random notes
+ *Example:* 
+ `/save notename
+ Reply 1
+ %%%
+ Reply 2
+ %%%
+ Reply 3`
+ ❍ /clear <notename>*:* clear note with this name
+ ❍ /clearall*:* removes all notes from the group
+ *Note:* Note names are case-insensitive, and they are automatically converted to lowercase before getting saved.
+"""
+
+__name__ = "notes"
+CMD_HELP.update({__name__: [__name__, __help__]})
