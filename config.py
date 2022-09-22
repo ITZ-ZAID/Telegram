5710e164b16844d701ceb77ee63766147788cb5b
Zaid <itzzaid@pm.me>
2022-09-22 08:16:55 +0530

    Intresting.

---
+++ config.py
@@ -0,0 +1,11 @@
+"""Config fill properly"""
+import os
+
+API_ID = int(os.environ.get("API_ID", ""))
+API_HASH = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+BOT_TOKEN1 = os.environ.get("BOT_TOKEN1", "")
+MONGO_DB_URI = os.environ.get("MONGO_DB_URI", "")
+DB_URI = os.environ.get("ELEPHANT_SQL", "")
+OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+BOT_ID = int(os.environ.get("BOT_ID", ""))
