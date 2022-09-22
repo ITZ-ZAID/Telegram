207336f24366ab413904719fd6641542612a697e
Zaid <itzzaid@pm.me>
2022-09-22 18:47:30 +0530

    Update warns_db.py

--- Zaid/Plugins/mongodb/warns_db.py
+++ Zaid/Plugins/mongodb/warns_db.py
@@ -204,11 +204,3 @@ def get_warn_expire(chat_id):
     if _s:
         return _s.get("expire"), _s.get("expiretime")
     return False, 0
-
-
-def get_total_warns():
-    _chats = warns.find({})
-    _total = 0
-    for x in _chats:
-        _total += len(x["chat_id"])
-    return _total
