e61df09a6d0d2783d7fa17f2c502ea0215e0cdb7
Zaid <itzzaid@pm.me>
2022-09-22 08:10:25 +0530

    Delete Telegram directory

--- Telegram/langs/__init__.py
+++
@@ -1,6 +0,0 @@
-from Telegram.langs.language import langs
-
-get_string = langs.get_string
-reload_strings = langs.reload_strings
-get_languages = langs.get_languages
-get_language = langs.get_language
