7cfcbe023da07c0b7b6f5806a1f49c28b5530c5f
Zaid <itzzaid@pm.me>
2022-09-22 20:24:20 +0530

    Update misc.py

--- Zaid/Plugins/misc.py
+++ Zaid/Plugins/misc.py
@@ -126,6 +126,96 @@ async def aa(event):
     await event.reply(translate(f"User {name}'s ID is `{user_id}`.", event.chat_id))
 
 
+
+@Zbot(pattern="^/info ?(.*)")
+async def _info(e):
+    if not e.reply_to and not e.pattern_match.group(1):
+        if e.sender_id:
+            x_user = e.sender
+    elif e.reply_to:
+        reply_msg = await e.get_reply_message()
+        if not reply_msg.sender_id:
+            return
+        x_user = reply_msg.sender
+    elif e.pattern_match.group(1):
+        x_obj = e.text.split(None, 1)[1]
+        x_ov = x_obj.replace("-", "")
+        if x_ov.isnumeric():
+            x_obj = int(x_obj)
+        try:
+            x_user = await e.client.get_entity(x_obj)
+        except (TypeError, ValueError) as x:
+            return await e.reply(str(x))
+    if isinstance(x_user, Channel):
+        x_channel = await e.client(GetFullChannelRequest(x_user.username or x_user.id))
+        out_str = f"<b>Channel Info:</b>"
+        out_str += f"\n<b>Title:</b> {x_user.title}"
+        if x_user.username:
+            out_str += f"\n<b>Username:</b> @{x_user.username}"
+        out_str += f"\n<b>Chat ID:</b> <code>{x_user.id}</code>"
+        if x_user.verified:
+            out_str += "\n<b>Verified:</b> True"
+        if x_channel.full_chat.about:
+            out_str += f"\n\n<b>Bio:</b> <code>{x_channel.full_chat.about}</code>"
+        if len(x_channel.chats) == 2:
+            out_str += f"\n<b>Linked Chat:</b> {x_channel.chats[1].title}"
+            out_str += (
+                f"\n<b>Linked Chat ID:</b> <code>-100{x_channel.chats[1].id}</code>"
+            )
+        if x_channel.full_chat.participants_count > 999:
+            participants_count = human_format(x_channel.full_chat.participants_count)
+        else:
+            participants_count = x_channel.full_chat.participants_count
+        out_str += f"\n\n<b>Participants:</b> <code>{participants_count}"
+        if x_channel.full_chat.admins_count:
+            out_str += f"\n<b>Admins:</b> <code>{x_channel.full_chat.admins_count}"
+        file = x_channel.full_chat.chat_photo
+        if isinstance(file, PhotoEmpty):
+            file = None
+        await e.reply(out_str, file=file, parse_mode="html")
+    elif isinstance(x_user, User):
+        x_full = await tbot(GetFullUserRequest(x_user.username or x_user.id))
+        out_str = "<b>User Info:</b>"
+        out_str += f"\n<b>First Name:</b> {x_full.user.first_name}"
+        if x_full.user.last_name:
+            out_str += f"\n<b>Last Name:</b> {x_full.user.last_name}"
+        if x_full.user.username:
+            out_str += f"\n<b>Username:</b> @{x_full.user.username}"
+        out_str += f"\n<b>User ID:</b> <code>{x_full.user.id}</code>"
+        out_str += (
+            f"\n<b>PermaLink:</b> <a href='tg://user?id={x_full.user.id}'>link</a>"
+        )
+        if x_full.profile_photo and x_full.profile_photo.dc_id:
+            out_str += f"\n<b>DC ID:</b> {x_full.profile_photo.dc_id}"
+        if x_full.about:
+            out_str += f"\n\n<b>Bio:</b> <code>{x_full.about}</code>"
+        x_about = user_about_x.find_one({"user_id": x_full.user.id})
+        if x_about:
+            out_str += f"\n\n<b>What others Say:</b> <code>{x_about['about']}</code>"
+        if x_full.user.id == OWNER_ID:
+            out_str += f"\n\nThis is my Master, he have total power over me!"
+        elif x_full.user.id in DEVS:
+            out_str += f"\n\n<b>Status:</b> Commited(Dev)."
+        elif x_full.user.id in SUDO_USERS:
+            out_str += f"\n\n<b>Status:</b> Single(sudo)."
+        if (
+            not x_full.user.id in DEVS
+            and not x_full.user.id in SUDO_USERS
+            and not x_full.user.id == OWNER_ID
+            and not x_full.user.id == BOT_ID
+        ):
+            if gbanned.find_one({"user": x_full.user.id}):
+                x_gbanned = "Yes"
+            else:
+                x_gbanned = "No"
+            if x_full.about:
+                out_str += f"\n\n<b>Gbanned:</b> {x_gbanned}"
+            else:
+                out_str += f"\n<b>Gbanned:</b> {x_gbanned}"
+            out_str += f"\n\n<b>BlackListed:</b> No"
+        await e.reply(out_str, file=x_full.profile_photo, parse_mode="html")
+
+
 @Zbot(pattern="^/bin ?(.*)")
 async def bin(event):
     if event.reply_to_msg_id:
