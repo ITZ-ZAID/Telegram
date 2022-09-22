015eb5721d30181010a3a9875b40a8ccb40c7fdb
Zaid <itzzaid@pm.me>
2022-09-22 21:05:12 +0530

    Update misc.py

--- Zaid/Plugins/misc.py
+++ Zaid/Plugins/misc.py
@@ -190,19 +190,8 @@ async def _info(e):
             out_str += f"\n<b>DC ID:</b> {x_full.profile_photo.dc_id}"
         if x_full.about:
             out_str += f"\n\n<b>Bio:</b> <code>{x_full.about}</code>"
-        x_about = user_about_x.find_one({"user_id": x_full.user.id})
-        if x_about:
-            out_str += f"\n\n<b>What others Say:</b> <code>{x_about['about']}</code>"
         if x_full.user.id == OWNER_ID:
             out_str += f"\n\nThis is my Master, he have total power over me!"
-        if gbanned.find_one({"user": x_full.user.id}):
-            x_gbanned = "Yes"
-        else:
-            x_gbanned = "No"
-        if x_full.about:
-            out_str += f"\n\n<b>Gbanned:</b> {x_gbanned}"
-        else:
-            out_str += f"\n<b>Gbanned:</b> {x_gbanned}"
         out_str += f"\n\n<b>BlackListed:</b> No"
         await e.reply(out_str, file=x_full.profile_photo, parse_mode="html")
 
