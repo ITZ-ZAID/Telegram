1d2e257ac9d72d5a511990a680e607bbcf31c764
Zaid <itzzaid@pm.me>
2022-09-22 20:18:22 +0530

    Update blocklist.py

--- Zaid/Plugins/blocklist.py
+++ Zaid/Plugins/blocklist.py
@@ -304,3 +304,28 @@ async def bl_warn(chat_id, first_name, user_id, reply_id, name):
             ),
             reply_to=reply_id,
         )
+
+
+__name__ = "blocklist"
+
+__help__ = """
+Blacklists are used to stop certain triggers from being said in a group. Any time the trigger is mentioned, the message will immediately be deleted. A good combo is sometimes to pair this up with warn filters!
+*NOTE*: Blacklists do not affect group admins.
+ ❍ /blacklist*:* View the current blacklisted words.
+Admin only:
+ ❍ /addblacklist <triggers>*:* Add a trigger to the blacklist. Each line is considered one trigger, so using different lines will allow you to add multiple triggers.
+ ❍ /unblacklist <triggers>*:* Remove triggers from the blacklist. Same newline logic applies here, so you can remove multiple triggers at once.
+ ❍ /blacklistmode <off/del/warn/ban/kick/mute/tban/tmute>*:* Action to perform when someone sends blacklisted words.
+Blacklist sticker is used to stop certain stickers. Whenever a sticker is sent, the message will be deleted immediately.
+*NOTE:* Blacklist stickers do not affect the group admin
+ ❍ /blsticker*:* See current blacklisted sticker
+*Only admin:*
+ ❍ /addblsticker <sticker link>*:* Add the sticker trigger to the black list. Can be added via reply sticker
+ ❍ /unblsticker <sticker link>*:* Remove triggers from blacklist. The same newline logic applies here, so you can delete multiple triggers at once
+ ❍ /rmblsticker <sticker link>*:* Same as above
+ ❍ /blstickermode <ban/tban/mute/tmute>*:* sets up a default action on what to do if users use blacklisted stickers
+Note:
+ ❍ <sticker link> can be `https://t.me/addstickers/<sticker>` or just `<sticker>` or reply to the sticker message
+"""
+from .. import CMD_HELP
+CMD_HELP.update({__name__: [__name__, __help__]})
