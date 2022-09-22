3a9ca4ca67244bc1877b4418cf72ee9195ae9f56
Zaid <itzzaid@pm.me>
2022-09-22 08:20:35 +0530

    Create __init__.py

---
+++ Zaid/__init__.py
@@ -0,0 +1,8 @@
+from config import API_ID, API_HASH, BOT_TOKEN
+from telethon import TelegramClient
+import telegram.ext as tg 
+babe = TelegramClient('Melody', api_id=API_ID, api_hash=API_HASH)
+Zaid = babe.start(bot_token=BOT_TOKEN)
+updater = tg.Updater(BOT_TOKEN, workers=8, use_context=True)
+dispatcher = updater.dispatcher
+updater.start_polling()
