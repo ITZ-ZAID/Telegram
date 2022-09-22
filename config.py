cc658c50c77fdc26c26e91cdc723b4231db38c01
Zaid <itzzaid@pm.me>
2022-09-22 09:26:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,6 @@ import os
 API_ID = int(os.environ.get("API_ID", ""))
 API_HASH = os.environ.get("API_HASH", "")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-BOT_TOKEN1 = os.environ.get("BOT_TOKEN1", "")
 MONGO_DB_URI = os.environ.get("MONGO_DB_URI", "")
 DB_URI = os.environ.get("ELEPHANT_SQL", "")
 OWNER_ID = int(os.environ.get("OWNER_ID", ""))
