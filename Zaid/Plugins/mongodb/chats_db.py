85df5361a60e20be3929f7cac324ae51bc087d8f
Zaid <itzzaid@pm.me>
2022-09-22 09:06:24 +0530

    Create chats_db.py

---
+++ Zaid/Plugins/mongodb/chats_db.py
@@ -0,0 +1,41 @@
+from . import db
+
+chats = db.chats
+
+
+def add_chat(chat_id: int):
+    _chats = chats.find_one({"type": "main"})
+    if _chats:
+        c = _chats["chats"]
+    else:
+        c = []
+    c.append(chat_id)
+    chats.update_one({"type": "main"}, {"$set": {"chats": c}}, upsert=True)
+
+
+def rm_chat(chat_id: int):
+    _chats = chats.find_one({"type": "main"})
+    if _chats:
+        c = _chats["chats"]
+    else:
+        c = []
+    if chat_id in c:
+        c.remove(chat_id)
+    chats.update_one({"type": "main"}, {"$set": {"chats": c}}, upsert=True)
+
+
+def get_all_chat_id():
+    _chats = chats.find_one({"type": "main"})
+    if _chats:
+        return _chats["chats"]
+    return None
+
+
+def is_chat(chat_id: int):
+    _chats = chats.find_one({"type": "main"})
+    if not _chats:
+        return False
+    elif chat_id in _chats["chats"]:
+        return True
+    else:
+        return False
