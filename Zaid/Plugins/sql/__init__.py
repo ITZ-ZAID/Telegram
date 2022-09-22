490dd68c25e0298190cdcc0c262ae94c668abf4e
Zaid <itzzaid@pm.me>
2022-09-22 09:14:53 +0530

    Create __init__.py

---
+++ Zaid/Plugins/sql/__init__.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+from config import DB_URI
+
+
+
+def start() -> scoped_session:
+    engine = create_engine(DB_URI, client_encoding="utf8")
+    BASE.metadata.bind = engine
+    BASE.metadata.create_all(engine)
+    return scoped_session(sessionmaker(bind=engine, autoflush=False))
+
+
+BASE = declarative_base()
+SESSION = start()
