9c483a83fd118ed8a7cbc80789ac0d4de71d7399
Zaid <itsunknown122@gmail.com>
2022-12-17 13:23:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,6 +35,10 @@ class Config(object):
     BAN_STICKER = getenv("BAN_STICKER", "")
     ALLOW_EXCL = True
     CUSTOM_CMD = False
+    CHANNEL = getenv("CHANNEL", "TheUpdatesChannel")
+    SUPPORT = getenv("SUPPORT", "TheSupportChat")
+    START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/35a7b5d9f1f2605c9c0d3.png")
+    CMD_IMG = os.environ.get("CMD_IMG", "https://telegra.ph/file/66518ed54301654f0b126.png")
     CASH_API_KEY = getenv("CASH_API_KEY", "https://www.alphavantage.co/support/#api-key")
     TIME_API_KEY = getenv("TIME_API_KEY", "https://timezonedb.com/api")
     WALL_API = getenv("WALL_API", "https://wall.alphacoders.com/api.php")
